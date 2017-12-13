0b71ab334c5a9120808c655c99d6d468e9e78de8
thefuentescore <sergio.softdevelop@gmail.com>
2017-12-13 10:34:09 +0100

    Login and register styling

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,9 +1,11 @@
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
 import { LoginPage } from './../login/login';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { UserData } from '../../models/user';
 import { UserServiceProvider } from '../../providers/user-service/user-service';
 import { ToastServiceProvider } from '../../providers/toast-service/toast-service';
+import { Loading } from 'ionic-angular/components/loading/loading';
 /**
  * Generated class for the RegisterPage page.
  *
@@ -20,14 +22,27 @@ export class RegisterPage {
   email: string;
   password: string;
 
-  constructor(private userService: UserServiceProvider, public navCtrl: NavController, public navParams: NavParams, private toast: ToastServiceProvider) {}
+  loading : Loading;
+  constructor(
+    private userService: UserServiceProvider, 
+    public navCtrl: NavController,
+     public navParams: NavParams, 
+     private toast: ToastServiceProvider, 
+     private loadingCtrl: LoadingController) {}
 
   register() {
+    this.showLoading();
     this.userService.register(this.email, this.password)
     .then(() => {
-      this.navCtrl.push(LoginPage);
+      this.navCtrl.popToRoot();
     });
   }
-
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
 }
 
